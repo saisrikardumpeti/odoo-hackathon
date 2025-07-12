19452e07b0e9a4452b925e18c1bc8fa324c306a9
saisrikardumpeti <saisrikardumpeti@gmail.com>
2025-07-12 17:02:10 +0530

    smalll changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import { authConfig } from './config/auth';
 import { ExpressAuth, getSession } from '@auth/express';
-import { authenticatedUser } from './middleware/auth.middleware';
 import router from './routes';
 
 
@@ -12,8 +11,7 @@ app.set('trust proxy', true)
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth/*", ExpressAuth(authConfig))
-app.use("/api*", router)
-
+app.use("/api/", router)
 
 
 export default app;
